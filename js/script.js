9b0abf2b54f0663bdab9c46ad52fbc8d54393c09
Bino000 <marcosbino.123@gmail.com>
2026-04-09 01:47:11 +0000

    Adicionar abas CEP, RUA e LOG com busca por endereço e exibição de resultados em tabela

--- js/script.js
+++ js/script.js
@@ -19,4 +19,59 @@ function mostrar(){
 	// FIM DA IMPLEMENTAÇÃO DO FETCH
 	console.log("Oi, meu CEP É fora", cep)
 }
+
+function buscarPorEndereco(){
+	uf = document.getElementById("uf").value
+	cidade = document.getElementById("cidade_search").value
+	logradouro = document.getElementById("logradouro").value
+	url = `https://viacep.com.br/ws/${uf}/${cidade}/${logradouro}/json/`
+
+	fetch(url)
+		.then((res)=>{
+			return res.json()
+		})
+		.then((data)=>{
+			console.log("Resultados da busca por endereço", data)
+			displayResults(data)
+			// Switch to log-tab
+			$('.tabs').tabs('select', 'log-tab');
+		})
+}
+
+function displayResults(data){
+	const resultadosDiv = document.getElementById("resultados")
+	resultadosDiv.innerHTML = "" // Clear previous results
+	if (Array.isArray(data) && data.length > 0) {
+		const table = document.createElement("table")
+		table.className = "striped"
+		table.innerHTML = `
+			<thead>
+				<tr>
+					<th>CEP</th>
+					<th>Logradouro</th>
+					<th>Bairro</th>
+					<th>Cidade</th>
+					<th>UF</th>
+				</tr>
+			</thead>
+			<tbody>
+			</tbody>
+		`
+		const tbody = table.querySelector("tbody")
+		data.forEach(item => {
+			const row = document.createElement("tr")
+			row.innerHTML = `
+				<td>${item.cep}</td>
+				<td>${item.logradouro}</td>
+				<td>${item.bairro}</td>
+				<td>${item.localidade}</td>
+				<td>${item.uf}</td>
+			`
+			tbody.appendChild(row)
+		})
+		resultadosDiv.appendChild(table)
+	} else {
+		resultadosDiv.innerHTML = "<p>Erro na busca ou nenhum resultado encontrado.</p>"
+	}
+}
 // tag fechamento do script JS
